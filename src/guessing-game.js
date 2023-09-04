9b4f881a50b2aa4bbffa85e655faa82285c9f962
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2023-09-04 20:17:46 +0300

    feat: task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0;
+        this.min = 0
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.number = Math.ceil(this.min + (this.max - this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.number;
     }
 
     greater() {
-
+        this.min = this.number;
     }
 }
 
